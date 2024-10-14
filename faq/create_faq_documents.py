797e26ab17fb54c573e882e7747d58c82b9b673a
XIE,JIA-HONG <64515959+JIAHONG0824@users.noreply.github.com>
2024-10-14 13:25:40 +0800

    run this file to get documents of faq

---
+++ faq/create_faq_documents.py
@@ -0,0 +1,18 @@
+import json
+with open("reference/faq/pid_map_content.json","rb") as f:
+    datas=json.load(f)
+documents=[]
+for i in range(len(datas)):
+    for qa in datas[str(i)]:
+        documents.append(
+            {
+                "text":qa["question"],
+                "metadata":{
+                    "question":qa["question"],
+                    "answers":qa["answers"],
+                    "source_id":i,
+                }
+            }
+        )
+with open("faq/faq_documents.json","w",encoding="utf8") as f:
+    json.dump(documents,f,ensure_ascii=False,indent=4)
