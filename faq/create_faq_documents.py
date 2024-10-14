7e1694ecda5181e09942bc31ca9d4792bc001003
XIE,JIA-HONG <64515959+JIAHONG0824@users.noreply.github.com>
2024-10-14 14:23:10 +0800

    ...

--- faq/create_faq_documents.py
+++ faq/create_faq_documents.py
@@ -1,18 +1,19 @@
 import json
-with open("reference/faq/pid_map_content.json","rb") as f:
-    datas=json.load(f)
-documents=[]
+
+with open("reference/faq/pid_map_content.json", "rb") as f:
+    datas = json.load(f)
+documents = []
 for i in range(len(datas)):
     for qa in datas[str(i)]:
         documents.append(
             {
-                "text":qa["question"],
-                "metadata":{
-                    "question":qa["question"],
-                    "answers":qa["answers"],
-                    "source_id":i,
-                }
+                "text": qa["question"],
+                "metadata": {
+                    "question": qa["question"],
+                    "answers": qa["answers"],
+                    "id": i,
+                },
             }
         )
-with open("faq/faq_documents.json","w",encoding="utf8") as f:
-    json.dump(documents,f,ensure_ascii=False,indent=4)
+with open("faq/faq_documents.json", "w", encoding="utf8") as f:
+    json.dump(documents, f, ensure_ascii=False, indent=4)
